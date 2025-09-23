01acfba64d183ba012d3edbd61507b88b78abeaa
Kelia Simbi <simbikelia@gmail.com>
2025-09-23 04:25:49 +0200

    Feat: Change colors of cards

--- family/schemaTypes/index.ts
+++ family/schemaTypes/index.ts
@@ -2,6 +2,7 @@ import post from './post'
 import author from './author'
 import blockContent from './blockContent'
 import images from './images';
+import birthday from './birthday';
 
 
-export const schemaTypes = [post, author, blockContent, images];
+export const schemaTypes = [post, author, blockContent, images, birthday];
