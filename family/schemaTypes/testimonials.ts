c1b79f60d00286a40d5aefe4c2b44da50cc68872
Kelia Simbi <simbikelia@gmail.com>
2025-10-13 16:48:53 +0200

    Fix: testimonials issue

--- family/schemaTypes/testimonials.ts
+++ family/schemaTypes/testimonials.ts
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'story',
-  title: 'Story',
+  name: 'testimonials',
+  title: 'Testimonials',
   type: 'document',
 
   fields: [
