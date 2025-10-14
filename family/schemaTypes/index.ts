c70bf10ca6b651238c90ef3f1d7ee1fd56c478df
Kelia Simbi <simbikelia@gmail.com>
2025-10-14 11:20:07 +0200

    Import the about schema

--- family/schemaTypes/index.ts
+++ family/schemaTypes/index.ts
@@ -5,6 +5,7 @@ import images from './images';
 import birthday from './birthday';
 import story from './story';
 import testimonials from './testimonials';
+import about from './about';
 
 
-export const schemaTypes = [post, author, blockContent, images, birthday, story, testimonials];
+export const schemaTypes = [post, author, blockContent, images, birthday, story, testimonials, about];
