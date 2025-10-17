eddb1352c225daef909662e5543377d210dbc79f
NyLaurent <laulanyumbayire@gmail.com>
2025-10-17 15:20:11 -0500

    feat: all members

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Duplicates from "./pages/Blogs/All";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
 import Birthdays from "./pages/Birthdays";
+import Members from "./pages/Members";
 import Header from "./components/Header";
 import SignUp from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
@@ -112,6 +113,15 @@ function AppRoutes() {
             element={
               <RequireAuth>
                 <Birthdays />
+              </RequireAuth>
+            }
+          />
+
+          <Route
+            path="/members"
+            element={
+              <RequireAuth>
+                <Members />
               </RequireAuth>
             }
           />
